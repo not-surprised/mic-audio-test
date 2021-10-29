24dcdd02fb3ba11e9364969b3d458912dcf0217a
Amanda Chen <chenamanda256@gmail.com>
2021-10-29 16:12:06 -0400

    added methods to convert from sound pressure to decibels (only for printing currenctly)

--- mic_no_graph.py
+++ mic_no_graph.py
@@ -1,9 +1,25 @@
+import math
+
 import sounddevice as sd
+import numpy as np
+
+def rms(arr): # calculates root mean squared of an array of sound data
+    rms = np.sqrt(np.mean(arr ** 2))
+    return rms
+
+def toDecibels(pressure):
+    p = rms(pressure)
+    pp0 = abs(p / 0.00002) # rms / threshold
+    if (pp0 > 0.00000001): # ensures the value is not too close to 0 (or is 0)
+        decibels = 20 * math.log(pp0 , 10) # converstion to decibels
+    else:
+        decibels = 0
+    return decibels
 
 def callback(indata, outdata, frames, time, status):
     if status:
         print(status)
-    print(indata)
+    print(toDecibels(indata))
 
 with sd.Stream(channels=1, callback=callback):
     while sd.Stream.active:
