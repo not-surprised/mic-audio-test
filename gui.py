f4c0ba5954125172b4e6dea071728a149803f68a
Amanda Chen <chenamanda256@gmail.com>
2021-11-12 16:44:45 -0500

    more gui stuff

--- gui.py
+++ gui.py
@@ -1,40 +1,46 @@
-import PySimpleGUI as sg
+import PySimpleGUIQt as sg
 
-num_displays = 1
+num_displays = 3
 num_audio = 2
 
 font = "Helvetica"
-header_size = "60"
-body_size = "36"
+header_size = "36"
+body_size = "14"
+
+sg.theme("Dark")
 
 full_size = (800, 500)
 col_size = (375, 400)
 
+monitor_image = "monitor_image.png"
 
-left_col = [[sg.Text("Displays", font=(font + "," +header_size))]]
+left_col = [[sg.Text("Displays", font=(font + " " +header_size))]]
 for i in range(num_displays):
-    left_col.append([sg.Image(sg.EMOJI_BASE64_SAD)])
-    left_col.append([sg.Text("Display", font=(font + "," +body_size))])
-    left_col.append([sg.Checkbox("Enable !surprised", font=(font + "," +body_size))])
+    left_col.append([sg.Image(monitor_image, size=(50, 50))])
+    left_col.append([sg.Text("Display", font=(font + " " +body_size))])
+    left_col.append([sg.Checkbox("Enable !surprised", font=(font + " " +body_size))])
 
-right_col = [[sg.Text("Audio", font=(font + "," +header_size))]]
+right_col = [[sg.Text("Audio", font=(font + " " +header_size))]]
 for i in range(num_audio):
-    right_col.append([sg.Text("Audio Device", font=(font + "," +body_size))])
-    right_col.append([sg.Checkbox("Enable !surprised", font=(font + "," +body_size))])
+    right_col.append([sg.Text("Audio Device", font=(font + " " +body_size))])
+    right_col.append([sg.Checkbox("Using headphones", font=(font + " " +body_size))])
+    right_col.append([sg.Checkbox("Enable !surprised", font=(font + " " +body_size))])
 
-calibrate_button = [sg.Button("Calibrate", font=(font + "," +body_size))]
-is_headphones = [sg.Checkbox("Using headphones", font=(font + "," +body_size))]
-
-button_container = [calibrate_button, is_headphones]
+calibrate_button = [sg.Button('Calibrate', enable_events=True)]
 
 
-layout = [[sg.Column(left_col, element_justification='c', size=col_size), sg.Column(right_col, element_justification='c', size=col_size)],
+button_container = [calibrate_button]
+
+
+
+
+layout = [[sg.Column(left_col, element_justification='l', size=col_size), sg.Column(right_col, element_justification='r', size=col_size)],
           [sg.Column(button_container, element_justification='r', size = (75, 50))]]
 
 window = sg.Window("!surprised", layout, size=full_size)
 
 while True:
-    event = window.read()
+    event, values  = window.read()
     # End program if user closes window or
     # presses the OK button
     if event == "Calibrate" or event == sg.WIN_CLOSED:
