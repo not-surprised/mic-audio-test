45b5430704aa6dd87cd0396fbeac77cb563e6244
Amanda <chenamanda256@gmail.com>
2021-11-16 17:48:33 -0500

    gui formatting changes added image for speakers fixed spacing and some buttons now scrollable

--- gui.py
+++ gui.py
@@ -1,43 +1,58 @@
 import PySimpleGUIQt as sg
 
 num_displays = 3
-num_audio = 2
+num_audio = 4
 
 font = "Helvetica"
-header_size = "36"
-body_size = "14"
+header_size = "24"
+body_size = "13"
+small_body_size = "9"
 
 sg.theme("Dark")
 
-full_size = (800, 500)
-col_size = (375, 400)
+full_size = (1400, 1000) # width, height
+col_size = (600, 450)
 
 monitor_image = "monitor_image.png"
+speaker_image = "speaker_image.png"
 
-left_col = [[sg.Text("Displays", font=(font + " " +header_size))]]
+padding = [[sg.Stretch()]]
+
+left_col = [[sg.Text("\nDisplays\n", font=(font + " " +header_size))]]
+
 for i in range(num_displays):
     left_col.append([sg.Image(monitor_image, size=(50, 50))])
-    left_col.append([sg.Text("Display", font=(font + " " +body_size))])
-    left_col.append([sg.Checkbox("Enable !surprised", font=(font + " " +body_size))])
+    left_col.append([sg.Text("\nDisplay", font=(font + " " +body_size))])
+    left_col.append([sg.Text("\n", font=(font + " " + "2"))])
+    left_col.append([sg.Checkbox("Enable !surprised", font=(font + " " +small_body_size))])
+    left_col.append([sg.Text("\n\n", font=(font + " " + body_size))])
 
-right_col = [[sg.Text("Audio", font=(font + " " +header_size))]]
+
+right_col = [[sg.Text("\nAudio\n", font=(font + " " +header_size))]]
+
 for i in range(num_audio):
-    right_col.append([sg.Text("Audio Device", font=(font + " " +body_size))])
-    right_col.append([sg.Checkbox("Using headphones", font=(font + " " +body_size))])
-    right_col.append([sg.Checkbox("Enable !surprised", font=(font + " " +body_size))])
+    right_col.append([sg.Image(speaker_image, size=(50, 50))])
+    right_col.append([sg.Text("\nAudio Device", font=(font + " " +body_size))])
+    left_col.append([sg.Text("\n", font=(font + " " + "2"))])
+    right_col.append([sg.Checkbox("Using headphones", font=(font + " " +small_body_size))])
+    right_col.append([sg.Checkbox("Enable !surprised", font=(font + " " +small_body_size))])
+    right_col.append([sg.Text("\n", font=(font + " " + body_size))])
 
-calibrate_button = [sg.Button('Calibrate', font=(font + " " +body_size), pad=(25, 25))]
+
+calibrate_button = [sg.Button('Calibrate', font=(font + " " + body_size), size=(140, 60), button_color=("#bac6d4", "#3f618a"))]
 
 
 button_container = [calibrate_button]
 
+padding = [[sg.Stretch()]]
+
+layout = [[sg.Stretch(), sg.Column(left_col, element_justification='l'), sg.Stretch(), sg.Stretch(), sg.Column(right_col, element_justification='l'), sg.Stretch()],
+          [sg.Column(button_container, element_justification='r')]]
+scrollable = [[sg.Column(layout, size=full_size, scrollable=True)]]
+window = sg.Window("!surprised", scrollable, size=full_size, resizable=False)
 
 
 
-layout = [[sg.Column(left_col, element_justification='l', size=col_size), sg.Column(right_col, element_justification='r', size=col_size)],
-          [sg.Column(button_container, element_justification='r', size = (75, 50))]]
-
-window = sg.Window("!surprised", layout, size=full_size)
 
 while True:
     event, values  = window.read()
